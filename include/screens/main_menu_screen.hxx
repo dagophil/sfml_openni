c838f9d34c73877f113343d92adcc7e3b63e4e57
Philip Schill <philip.schill@gmx.de>
2016-03-02 15:37:47 +0100

    Minor changes.

--- include/screens/main_menu_screen.hxx
+++ include/screens/main_menu_screen.hxx
@@ -19,13 +19,7 @@ public:
           event_manager_(event_manager),
           mouse_(mouse)
     {
-        auto w = 240;
-        auto h = 80;
-        auto x = (rect_.GetWidth()-w)/2;
-        auto gap = 20;
-        auto n = 5;
-        auto y = (rect_.GetHeight() - n*h - (n-1)*gap) / 2;
-
+        // Fill the vector with the images and the according events.
         std::vector<std::pair<std::string, Event> > v {
             {"images/start_button.png", Event::ChangeScreenEvent(Event::GameScreen)},
             {"images/highscore_button.png", Event::ChangeScreenEvent(Event::HighscoreScreen)},
@@ -34,19 +28,29 @@ public:
             {"images/exit_button.png", Event(Event::Close)}
         };
 
+        // Some variables for the button positions.
+        auto h_gap = rect_.GetHeight() / (v.size()+1.0);
+        auto h = 0.8*h_gap;
+        auto gap = 0.2*h_gap;
+        auto w = 2.76*h;
+        auto x = (rect_.GetWidth()-w)/2;
+        auto y0 = (rect_.GetHeight() - v.size()*(h+gap) + gap) / 2;
+
+        // Draw the buttons.
         for (size_t i = 0; i < v.size(); ++i)
         {
-            auto const & p = v[i];
+            auto const & img = v[i].first;
+            auto const & ev = v[i].second;
             auto btn = std::make_shared<HoverclickWidget<ImageWidget> >(
-                        p.first,
-                        x, y + i*(h+gap),
+                        img,
+                        x, y0 + i*h_gap,
                         w, h,
                         1
             );
             attach_mouse_events(mouse_, btn);
             add_widget(btn);
-            btn->handle_click_ = [&, p](DiffType x, DiffType y){
-                event_manager.post(p.second);
+            btn->handle_click_ = [&, ev](DiffType x, DiffType y){
+                event_manager.post(ev);
             };
         }
     }
